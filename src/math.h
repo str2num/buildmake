6b644c10bc3a298beec6fc2748bbcaaf3666b95f
zzp <aries@AY140531154838041015Z.(none)>
2017-11-07 20:18:16 +0800

    add source

---
+++ src/math.h
@@ -0,0 +1,27 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2017 str2num.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * @file math.h
+ * @author str2num
+ * @version $Revision$ 
+ * @brief 
+ *  
+ **/
+
+
+#ifndef  __BUILDMAKE_TEST_MATH_H_
+#define  __BUILDMAKE_TEST_MATH_H_
+
+int add(int a, int b);
+int sub(int a, int b);
+
+#endif  //__BUILDMAKE_TEST_MATH_H_
+
+
