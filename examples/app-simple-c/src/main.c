cc966d906af525e12175c59ea2ec8655cf38cd5d
str2num <str2num@str2num.com>
2018-07-21 19:18:12 +0800

    add example app-simple-c

---
+++ examples/app-simple-c/src/main.c
@@ -0,0 +1,28 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2017 str2num.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * @file main.c
+ * @author str2num
+ * @version $Revision$ 
+ * @brief 
+ *  
+ **/
+
+#include <stdio.h>
+#include "math.h"
+
+int main() {
+    printf("hello world!\n");
+    printf("add(1, 1) = %d\n", add(1, 1));
+
+    return 0;
+}
+
+
